ea076c5c1784583c842b01057c1e8fd9b63de194
forckes <forckes@proton.me>
2023-03-25 21:21:48 +0200

    add profile

---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+import user from './user.json';
+import data from './data.json';
+import friends from './friends.json';
+import transactions from './transaction.json';
+
+export { user, data, friends, transactions };
